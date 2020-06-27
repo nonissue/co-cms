b19d46eafb9c563e2ba706324eb4958ae0e97520
nonissue <andy@nonissue.org>
2020-06-27 01:51:26 -0600

    Prisma data fetching works on users page

--- pages/users.js
+++ pages/users.js
@@ -1,44 +1,45 @@
 import { PrismaClient } from '@prisma/client';
+import Layout from '../components/layout';
 
 async function getUsers() {
-  const allUsers = await prisma.user.findMany({});
+  const allUsers = await prisma.user.findMany({ include: { lates: true } });
 
   console.dir(allUsers, { depth: null });
 }
 
+// Receives prisma users with lates
+// parse them from json
+// Map over the resulting users
 function Users({ users }) {
-  console.log(users);
-  return <div>Users</div>;
+  const userList = JSON.parse(users);
+  const test = console.log(userList);
+
+  return (
+    <Layout>
+      <h1> Users</h1>
+      {userList.map((user) => {
+        return (
+          <div key={user.id}>
+            <h3>{user.name}</h3>
+            <h4>{user.email}</h4>
+            {console.log(user.lates)}
+          </div>
+        );
+      })}
+    </Layout>
+  );
 }
 
-// export async function getStaticProps() {
-//   const res = await await prisma.user.findMany({});
-//   const json = await res.json();
+// Gets all users with static props
+// Also fetches corresponding user posts
+// Converts it to JSON before returning
 
-//   console.log(json);
-
-//   return {
-//     props: {
-//       users: json,
-//     },
-//   };
-// }
-
-// export const getServerSideProps = async () => {
-//   const prisma = new PrismaClient();
-//   const res = await prisma.user.findMany({});
-//   const json = JSON.stringify(res);
-//   // const feed = await res.json();
-//   return {
-//     props: { users: json },
-//   };
-// };
-
+// Does the returned array need to be json? Can we avoid converting it to JSON and then back?
 export const getStaticProps = async () => {
   const prisma = new PrismaClient();
-  const res = await prisma.user.findMany({});
-  const json = JSON.stringify(res);
-  // const feed = await res.json();
+  const res = await prisma.user.findMany({ include: { lates: true } });
+  const json = await JSON.stringify(res);
+
   return {
     props: { users: json },
   };
