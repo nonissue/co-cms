db8f9eb8420f10061788fed57323152cc82ad894
nonissue <andy@nonissue.org>
2020-06-27 01:05:22 -0600

    Add SSR page demo

---
+++ pages/ssr.js
@@ -0,0 +1,33 @@
+import { getSession } from 'next-auth/client';
+import Layout from '../components/layout';
+
+export default ({ session }) => (
+  <Layout>
+    <h1>Server Side Rendering</h1>
+    <p>
+      This page uses the universal <strong>getSession()</strong> method in{' '}
+      <strong>getServerSideProps()</strong>.
+    </p>
+    <p>
+      Using <strong>getSession()</strong> in{' '}
+      <strong>getServerSideProps()</strong> is the recommended approach if you
+      need to support server side rendering with authentication.
+    </p>
+    <p>
+      <strong>
+        Server side page rendering is not as fast as client side rendering
+      </strong>
+      , so any page that uses this approach will be slower than a page that only
+      uses the React Hook.
+    </p>
+    <p>This page does not require client side JavaScript.</p>
+  </Layout>
+);
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      session: await getSession(context),
+    },
+  };
+}
