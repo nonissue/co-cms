5da5dd6d14eb90928979e99f990bde3da4ac8059
nonissue <andy@nonissue.org>
2020-07-11 18:14:47 -0600

    Verify user session, send with post req

--- pages/late/create.js
+++ pages/late/create.js
@@ -1,16 +1,21 @@
 import { useState } from 'react';
 import Router from 'next/router';
+import { useSession } from 'next-auth/client';
 
 import Layout from '../../components/layout';
 import styles from './create.module.css';
 
 function Create() {
+  const [session, loading] = useSession();
+
   const [lateData, setLateData] = useState({
     title: '',
     url: '',
     shared: 'false',
     error: '',
   });
+
+  console.log(session);
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -24,7 +29,7 @@ function Create() {
       const response = await fetch('/api/late/create', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ title, url, shared }),
+        body: JSON.stringify({ title, url, shared, user: session.user }),
       });
 
       if (response.status !== 200) {
@@ -52,75 +57,80 @@ function Create() {
 
   return (
     <Layout>
-      {console.log(lateData.error)}
-      <div className={styles['create-late']}>
-        <h1>Create Late</h1>
-        {/* <div className={styles['form-error']}>
+      {/* Ugly nested ternary, fix? */}
+      {loading ? (
+        'Loading...'
+      ) : !session ? (
+        'Please sign in'
+      ) : (
+        <div className={styles['create-late']}>
+          <h1>Create Late</h1>
+          {/* <div className={styles['form-error']}>
           {lateData.error &&&& `Error: ${lateData.error}`}
         </div> */}
-        <form onSubmit={handleSubmit}>
-          <div className={styles['form-group-row']}>
-            <label htmlFor='title'>Title</label>
-            <div>
-              <input
-                type='text'
-                id='title'
-                name='title'
-                value={lateData.title}
-                className={`${
-                  lateData.error?.field === 'Title' &&
-                  styles['form-invalid-input']
-                }
+          <form onSubmit={handleSubmit}>
+            <div className={styles['form-group-row']}>
+              <label htmlFor='title'>Title</label>
+              <div>
+                <input
+                  type='text'
+                  id='title'
+                  name='title'
+                  value={lateData.title}
+                  className={`${
+                    lateData.error?.field === 'Title' &&
+                    styles['form-invalid-input']
+                  }
                   ${styles['create-late-input']}
                 }`}
-                onChange={(event) =>
-                  setLateData(
-                    Object.assign({}, lateData, { title: event.target.value })
-                  )
-                }
-              />
+                  onChange={(event) =>
+                    setLateData(
+                      Object.assign({}, lateData, { title: event.target.value })
+                    )
+                  }
+                />
+              </div>
+              <div className={styles['form-help-text']}>
+                Enter the Late's title (optional)
+              </div>
             </div>
-            <div className={styles['form-help-text']}>
-              Enter the Late's title (optional)
-            </div>
-          </div>
 
-          <div className={styles['form-group-row']}>
-            <label htmlFor='summary'>URL</label>
-            <div>
-              <input
-                type='text'
-                id='url'
-                name='url'
-                value={lateData.url}
-                className={`${
-                  lateData.error?.field === 'URL' &&
-                  styles['form-invalid-input']
-                }
+            <div className={styles['form-group-row']}>
+              <label htmlFor='summary'>URL</label>
+              <div>
+                <input
+                  type='text'
+                  id='url'
+                  name='url'
+                  value={lateData.url}
+                  className={`${
+                    lateData.error?.field === 'URL' &&
+                    styles['form-invalid-input']
+                  }
                   ${styles['create-late-input']}
                 }`}
-                onChange={(event) =>
-                  setLateData(
-                    Object.assign({}, lateData, { url: event.target.value })
-                  )
-                }
-              />
+                  onChange={(event) =>
+                    setLateData(
+                      Object.assign({}, lateData, { url: event.target.value })
+                    )
+                  }
+                />
+              </div>
+              <div
+                className={`${styles['form-help-text']} ${styles['form-inline-error']}`}
+              >
+                {/* show the validation error for set amount of time? */}
+                {lateData.error?.field === 'URL' ? (
+                  <div className={`${styles['form-inline-error']}`}>
+                    This field is required
+                  </div>
+                ) : (
+                  "Enter the Late's URL (required)"
+                )}
+              </div>
             </div>
-            <div
-              className={`${styles['form-help-text']} ${styles['form-inline-error']}`}
-            >
-              {/* show the validation error for set amount of time? */}
-              {lateData.error?.field === 'URL' ? (
-                <div className={`${styles['form-inline-error']}`}>
-                  This field is required
-                </div>
-              ) : (
-                "Enter the Late's URL (required)"
-              )}
-            </div>
-          </div>
 
-          {/* <div className={styles['form-control-row-body']}>
+            {/* <div className={styles['form-control-row-body']}>
             <label htmlFor='body'>Body</label>
             <div>
               <textarea
@@ -138,7 +148,7 @@ function Create() {
             </div>
           </div> */}
 
-          {/* <div className={styles['form-control-select']}>
+            {/* <div className={styles['form-control-select']}>
             <label htmlFor='body'>Categories</label>
             <div>
               <select
@@ -169,11 +179,14 @@ function Create() {
             </div>
           </div> */}
 
-          <div className={`${styles['form-group-row']}`}>
-            <button type='submit'>Submit Late</button>
-          </div>
-        </form>
-      </div>
+            <div className={`${styles['form-group-row']}`}>
+              <button type='submit'>Submit Late</button>
+            </div>
+          </form>
+        </div>
+      )}
+
+      {/* )} */}
     </Layout>
   );
 }
