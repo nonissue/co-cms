f964fc863a27b5e5d3d439a82864e009915c17d9
nonissue <andy@nonissue.org>
2020-07-14 18:49:49 -0600

    testing fallback

--- pages/late/[id].js
+++ pages/late/[id].js
@@ -42,7 +42,7 @@ export async function getStaticPaths() {
     paths: [{ params: { id: '1' } }],
     // Enable statically generating additional pages
     // For example: `/posts/3`
-    fallback: false,
+    fallback: true,
   };
 }
 
