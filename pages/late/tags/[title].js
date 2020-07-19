c2d328acadf0fc02ad1840ef19a7809dab8d2ab2
nonissue <andy@nonissue.org>
2020-07-19 03:34:50 -0600

    Add late/tags pages

---
+++ pages/late/tags/[title].js
@@ -0,0 +1,90 @@
+import { PrismaClient } from '@prisma/client';
+import { useRouter } from 'next/router';
+import Layout from '../../../components/layout';
+const prisma = new PrismaClient();
+
+const Tag = ({ data, loading = true, error }) => {
+  if (!data) {
+    if (loading) {
+      return <Layout>Loading...</Layout>;
+    }
+    return (
+      <Layout>
+        <h1>Error: Late not found</h1>
+        <br />
+        <code>Requested id: {error ? error : 'nope'}</code>
+      </Layout>
+    );
+  }
+
+  console.log(data);
+
+  const { title, lates } = JSON.parse(data);
+
+  // console.log(lateResult);
+
+  return (
+    <Layout>
+      <h3>{title}</h3>
+      <ul>
+        {lates.map((late) => {
+          return <li>{late.url}</li>;
+        })}
+      </ul>
+    </Layout>
+  );
+};
+
+// export async function getStaticPaths() {
+//   const allLates = await prisma.late.findMany({
+//     include: { owner: true },
+//   });
+
+//   const paths = allLates.map((late) => `/late/${late.id}`);
+
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
+
+// move this to api call that fetches late
+// then use SWR to call api?
+export const getServerSideProps = async (context) => {
+  // const test = await getSession(context);
+
+  const tagTitle = context.params.title;
+  let tagWithLates;
+
+  try {
+    tagWithLates = await prisma.tag.findOne({
+      where: {
+        title: tagTitle,
+      },
+      include: { lates: true },
+    });
+  } catch (err) {
+    console.log(err);
+    console.log("can't find that tag");
+    // return { props: { late: null, loading: false } };
+    throw new Error('Error: Error fetching late: ' + context.params.title);
+  }
+
+  let json;
+
+  // console.log(tagWithLates);
+
+  if (tagWithLates) {
+    json = await JSON.stringify(tagWithLates);
+  } else {
+    return {
+      props: { data: null, loading: false, error: context.params.title },
+    };
+  }
+
+  return {
+    props: { data: json, loading: false, error: false },
+  };
+};
+
+export default Tag;
---
+++ pages/late/tags/index.js
@@ -0,0 +1,12 @@
+import Layout from '../../../components/layout';
+
+const Tags = () => {
+  return (
+    <Layout>
+      <h1>Tags</h1>
+      <p>Tag list goes here</p>
+    </Layout>
+  );
+};
+
+export default Tags;
