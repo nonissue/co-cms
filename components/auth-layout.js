fe4f378c58ad3366abaa7a810a56e5c42febebf5
nonissue <andy@nonissue.org>
2020-07-30 16:31:03 -0600

    Cleanup / bump

--- components/auth-layout.js
+++ components/auth-layout.js
@@ -2,10 +2,12 @@ import Header from '../components/header';
 import Footer from '../components/footer';
 import Layoujt from '../components/layout';
 
-export default ({ children }) => {
+const AuthLayout = ({ children }) => {
   <>
     {/* <Header /> */}
     {/* <main>{children}</main> */}
     {/* <Footer /> */}
   </>;
 };
+
+export default AuthLayout;
